25ab6a50f6226d78a7efd42050994efbbe64023f
watagame <wataamewatagashi@users.noreply.github.com>
2019-08-28 19:42:50 +0900

    add serverStatus

--- discordbot.py
+++ discordbot.py
@@ -44,10 +44,11 @@ class server_process:
         self.server.terminate()
 
     def server_is_running(self):
-        if self.server is None:
-            return False
-        if self.server.poll() is not None:
+        if self.server is None: return False
+        if self.server.poll() is None:
             return True
+        elif self.server.poll() is not None:
+            return False
         else:
             return False
 
@@ -59,12 +60,13 @@ server = server_process(f_name)
 
 #活動監視
 @tasks.loop(seconds=1)
-async def server_checker(server):
+async def server_checker():
     while (True):
-        if server.server_is_running() is not True:
+        if server.server_is_running() is True:
+            await client.change_presence(activity=starting, status=discord.Status.online)
+        else:
             await client.change_presence(activity=idle, status=discord.Status.idle)
-            return
-    await asyncio.sleep(1)
+        await asyncio.sleep(1)
 
 # 起動時に動作する処理
 @client.event
@@ -73,6 +75,8 @@ async def on_ready():
     print('ログインしました')
     print(client.user.name)
     print(client.user.id)
+    server_checker.start()
+    #client.loop.create_task(server_checker())
     #await client.change_presence(activity=idle, status=discord.Status.idle)
 
 # メッセージ受信時に動作する処理
@@ -128,6 +132,10 @@ async def on_message(message):
     elif message.content == 'r.reset': #runnning変数初期化(通常は使用しないでください)
         await client.change_presence(activity=idle, status=discord.Status.idle)
         await message.channel.send('初期化しました')
+
+    #デバッグ用コマンドのためコメントアウト
+    #elif message.content == 'r.get':
+        #await message.channel.send('初期化しました')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
